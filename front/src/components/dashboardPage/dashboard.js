1dead71ebf787fb97c0457be522c78979ac0fa51
maya.shasho <maya.shasho6292@gmail.com>
2025-02-25 09:12:09 +0200

    delete notes

--- front/src/components/dashboardPage/dashboard.js
+++ front/src/components/dashboardPage/dashboard.js
@@ -1,5 +1,5 @@
 import './dashboard.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import FetchWithAuth from '../../utils/fetchWithAuth.js';
 import { useNavigate } from 'react-router-dom';
 import TransactionHistory from './transactionHistory/transactionHistory.js';
@@ -7,55 +7,71 @@ import CreateTransaction from './createTransaction/createTransaction.js';
 import ShowBalance from './balance/showBalance.js';
 
 const User = () => {
-    const [balance, setBalance] = useState(0);
-    const [transactions, setTransactions] = useState([]);
-    const [info, setInfo] = useState(0);
-    const navigate = useNavigate();
-
     useEffect(() => {
         const fetchDashboardData = async () => {
             try {
                 fetchDashboard();
                 fetchUserInfo();
-
             } catch (error) {
                 console.error('Error fetching dashboard data:', error);
             }
         };
 
         fetchDashboardData();
-    }, [navigate]);
+    }, [fetchDashboard, fetchUserInfo]);
 
-    const fetchDashboard = async () => {
-        const balanceResponse = await FetchWithAuth('http://localhost:8080/user/balance', {}, navigate);
+    const [balance, setBalance] = useState(0);
+    const [transactions, setTransactions] = useState([]);
+    const [info, setInfo] = useState(0);
+    const navigate = useNavigate();
+
+    const fetchDashboard = useCallback(async () => {
+        const balanceResponse = await FetchWithAuth(
+            'http://localhost:8080/user/balance',
+            {},
+            navigate
+        );
         if (balanceResponse.ok) {
             const balanceData = await balanceResponse.json();
             setBalance(balanceData.balance);
         }
-    };
+    }, [navigate]);
 
-    const fetchUserInfo = async () => {
-        const infoResponse = await FetchWithAuth('http://localhost:8080/user/info', {}, navigate);
+    const fetchUserInfo = useCallback(async () => {
+        const infoResponse = await FetchWithAuth(
+            'http://localhost:8080/user/info',
+            {},
+            navigate
+        );
         if (infoResponse.ok) {
             const infoData = await infoResponse.json();
             setInfo(infoData.info);
         }
-    };
+    }, [navigate]);
 
-    const fetchTransactionsHistory = async () => {
-        const transactionsResponse = await FetchWithAuth('http://localhost:8080/user/transaction', {}, navigate);
+    const fetchTransactionsHistory = useCallback(async () => {
+        const transactionsResponse = await FetchWithAuth(
+            'http://localhost:8080/user/transaction',
+            {},
+            navigate
+        );
         if (transactionsResponse.ok) {
             const transactionsData = await transactionsResponse.json();
             setTransactions(transactionsData.transactions);
         }
-    }
+    }, [navigate]);
 
     return (
         <div className="DashboardContainer">
             <div className="DashboardContainerCheck">
-                <h1 className='DashboardHeader'>Welcome, {info.first_name} {info.last_name}!</h1>
+                <h1 className="DashboardHeader">
+                    Welcome, {info.first_name} {info.last_name}!
+                </h1>
                 <ShowBalance balance={balance} />
-                <CreateTransaction fetchDashboard={fetchDashboard} fetchTransactionsHistory={fetchTransactionsHistory} />
+                <CreateTransaction
+                    fetchDashboard={fetchDashboard}
+                    fetchTransactionsHistory={fetchTransactionsHistory}
+                />
                 <TransactionHistory
                     userEmail={info.email}
                     transactions={transactions}
