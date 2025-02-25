4c47c4ea02f4c62ccdbd37b838324e268290d2a1
maya.shasho <maya.shasho6292@gmail.com>
2025-02-25 13:49:08 +0200

    add test route

--- back/server.js
+++ back/server.js
@@ -1,12 +1,12 @@
-import "./config.js"
-import "./cronJob.js";
+import './config.js';
+import './cronJob.js';
 
-import express from "express"
-import userRoutes from "./routes/user.js"
-import signupRoute from "./routes/signup.js"
-import loginRoute from "./routes/login.js"
-import mongoose from "mongoose"
-import { refreshToken } from "./controllers/refresh.js"
+import express from 'express';
+import userRoutes from './routes/user.js';
+import signupRoute from './routes/signup.js';
+import loginRoute from './routes/login.js';
+import mongoose from 'mongoose';
+import { refreshToken } from './controllers/refresh.js';
 import cors from 'cors';
 
 const app = express();
@@ -16,11 +16,13 @@ const port = 8080;
 
 app.use(express.json());
 
-app.use("/signup", signupRoute);
-app.use("/login", loginRoute);
-app.use("/user", userRoutes);
-
-app.post("/refresh", refreshToken);
+app.use('/signup', signupRoute);
+app.use('/login', loginRoute);
+app.use('/user', userRoutes);
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello world!' });
+});
+app.post('/refresh', refreshToken);
 
 app.listen(port, async () => {
     try {
